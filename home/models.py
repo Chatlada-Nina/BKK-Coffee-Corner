e7a08ceb6e1082a1d7437e499d293a95e49aef14
Chatlada Juntarawanichkul <chatladanine@gmail.com>
2025-01-27 12:02:22 +0000

    Create review model, install cloudinary and pillow

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from PIL import Image
 
 # A draft is defined as zero and published as one. (From the I think therefore I blog CL project)
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -11,6 +13,7 @@ class Post(models.Model):
     cafeinfo = models.TextField()
     excerpt = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cafe_posts")
+    featured_image = CloudinaryField('image', default='placeholder')
     address = models.TextField()
     phone = models.TextField()
     opentime = models.TextField()
@@ -24,3 +27,13 @@ class Post(models.Model):
 # Display class object as a string to improve readable for admin
     def __str__(self):
         return f"{self.cafename} | written by {self.author}"
+
+
+# Create Review model
+class Review(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="reviews")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviewer")
+    created_on = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    body = models.TextField()
+    image = models.ImageField(blank=True)
