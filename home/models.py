ce4494d675dd08eaffe9e64ded358c498bdeaa81
Chatlada Juntarawanichkul <chatladanine@gmail.com>
2025-01-27 09:22:29 +0000

    Add post model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# A draft is defined as zero and published as one. (From the I think therefore I blog CL project)
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+# Create Post Cafes model
+class Post(models.Model):
+    cafename = models.CharField(max_length=100, unique=True)
+    cafeinfo = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cafe_posts")
+    address = models.TextField()
+    phone = models.IntegerField()
+    opentime = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
