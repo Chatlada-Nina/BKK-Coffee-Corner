94eaf4843cca627938e97530da189725636354b4
Chatlada Juntarawanichkul <chatladanine@gmail.com>
2025-01-28 10:35:10 +0000

    Create base and home templates

--- home/views.py
+++ home/views.py
@@ -6,4 +6,5 @@ from .models import Post
 # Create a homepage with generic views credited by I think threfore I blog: CL project"
 class PostList(generic.ListView):
     queryset = Post.objects.order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "home/index.html"
+    paginate_by = 6
