55aac2e2eecc315761f13e19400f8e8ebbe39d0d
Chatlada Juntarawanichkul <chatladanine@gmail.com>
2025-01-28 12:20:37 +0000

    Create a single post view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic 
 from .models import Post
 
@@ -8,3 +8,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by("-created_on")
     template_name = "home/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`home.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`home.Post`.
+
+    **Template:**
+
+    :template:`home/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(request,
+        "home/post_detail.html",
+        {"post": post},
+    )
