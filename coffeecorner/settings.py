5143e655536c76f149036ab6b0e1b87c197c5874
Chatlada-Nina <chatladanine@gmail.com>
2025-01-29 12:58:22 +0100

    Add authentication and style form

--- coffeecorner/settings.py
+++ coffeecorner/settings.py
@@ -42,12 +42,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django_summernote',
     'cloudinary',
     'home',
     'about',
 ]
+# Add constants for Django could handle multiple sites from one database, redirection URLs to redirect us to home page after logged in and logged out.
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'coffeecorner.urls'
@@ -119,6 +128,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Inform Django that it no need email verification in this project
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
