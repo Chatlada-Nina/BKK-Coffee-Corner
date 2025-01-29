801e01bfa2c0f3bcd2932610566cda05bf555eab
Chatlada-Nina <chatladanine@gmail.com>
2025-01-29 15:15:21 +0100

    Add review form functionality and comfirmation message

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import ReviewForm
 
 # Create your views here.
 # Create a homepage with generic views credited by I think threfore I blog: CL project"
@@ -29,11 +31,26 @@ def post_detail(request, slug):
     reviews = post.reviews.all().order_by("-created_on")
     review_count = post.reviews.count()
 
+    if request.method == "POST":
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.author = request.user
+            review.post = post
+            review.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Review submitted! Thank you for sharing."
+            )
+
+    review_form = ReviewForm()
+
     return render(request,
         "home/post_detail.html",
         {
             "post": post,
             "reviews": reviews,
             "review_count": review_count,
+            "review_form": review_form,
         },
     )
