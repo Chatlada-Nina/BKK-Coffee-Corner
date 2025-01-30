a71f3888aacb9d7905db4736b16e1fb2c89da364
Chatlada-Nina <chatladanine@gmail.com>
2025-01-30 13:15:54 +0100

    Link forum page to navbar

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Forum, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+# Give the admin panel greater functionality and clarity.
+@admin.register(Forum)
+class ForumAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'created_on')
+    search_fields = ['title']
+    list_filter = ('created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Forum)
 admin.site.register(Comment)
