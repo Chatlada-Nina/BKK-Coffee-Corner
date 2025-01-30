215c0ec98f1b3c5bb4160de38c27f045b5be9bac
Chatlada-Nina <chatladanine@gmail.com>
2025-01-30 12:31:27 +0100

    Create generic forum view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Forum
 
 # Create your views here.
+class ForumList(generic.ListView):
+    queryset = Forum.objects.all()
+    template_name = "forum_list.html"
