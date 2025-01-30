33ccb96818b7f2271e715d57e024c3d672e0d7b4
Chatlada-Nina <chatladanine@gmail.com>
2025-01-30 13:38:44 +0100

    Add forum detail view

--- forum/urls.py
+++ forum/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ForumList.as_view(), name='forum'),
+    path('<slug:slug>/', views.forum_detail, name='forum_detail'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Forum
 
@@ -6,3 +6,27 @@ from .models import Forum
 class ForumList(generic.ListView):
     queryset = Forum.objects.all()
     template_name = "forum_list.html"
+
+
+def forum_detail(request, slug):
+    """
+    Display an individual :model:`forum.Forum`.
+
+    **Context**
+
+    ``forum``
+        An instance of :model:`forum.Forum`.
+
+    **Template:**
+
+    :template:`forum/forum_detail.html`
+    """
+
+    queryset = Forum.objects.order_by("created_on")
+    forum = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "forum/forum_detail.html",
+        {"forum": forum},
+    )
