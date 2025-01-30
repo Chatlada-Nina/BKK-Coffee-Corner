2905f33629550a322e9c11cef1339b58eec5b708
Chatlada-Nina <chatladanine@gmail.com>
2025-01-30 19:36:06 +0100

    Style forum page

--- coffeecorner/urls.py
+++ coffeecorner/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("forum/", include ("forum.urls"), name="forum-urls"),
+    path("forum/", include("forum.urls"), name="forum-urls"),
     path('', include("home.urls"), name="home-urls"),
 ]
