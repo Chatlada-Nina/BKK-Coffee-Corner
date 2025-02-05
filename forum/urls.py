e76c68722b8f81fb99b739c94eaeb355cf727baa
Chatlada-Nina <chatladanine@gmail.com>
2025-02-05 17:41:16 +0100

    Fix the delete forum confirmation functionality

--- forum/urls.py
+++ forum/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('new-forum/', views.forum_new, name="new-forum"),
     path('<slug:slug>/', views.forum_detail, name='forum-detail'),
     path('forum_edit/<int:forum_id>/', views.forum_edit, name='forum-edit'),
-    path('forum_delete/<int:forum_id>/', views.forum_delete, name='forum-delete'),
+    path('delete_forum/<int:forum_id>/', views.forum_delete, name='forum-delete'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
